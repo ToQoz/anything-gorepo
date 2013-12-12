1faf3fe4fc16a2b568548c3eaa8a5131a505f5a6
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-12-12 13:25:21 +0900

    Initial commit
    bundle gem anything-gorepo

---
+++ lib/anything/gorepo.rb
@@ -0,0 +1,7 @@
+require "anything/gorepo/version"
+
+module Anything
+  module Gorepo
+    # Your code goes here...
+  end
+end
---
+++ lib/anything/gorepo/version.rb
@@ -0,0 +1,5 @@
+module Anything
+  module Gorepo
+    VERSION = "0.0.1"
+  end
+end

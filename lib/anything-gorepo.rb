3ef4d33af0c56ca6c1761854e8ad88369c65c242
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-01-20 11:47:20 +0900

    Find in goroot and gopath

--- lib/anything-gorepo.rb
+++ lib/anything-gorepo.rb
@@ -2,7 +2,39 @@ require "anything-gorepo/version"
 require "anything-gorepo/finder"
 
 module AnythingGorepo
-  def self.find src
-    File.join(src, Finder.new(src).find)
+  def self.find paths
+    # key:   package name
+    # value: package full path
+    pkgs = {}
+
+    paths.each do |path|
+      packages(path).map do |pkg|
+        pkgs[pkg.gsub(path + '/', '')] = pkg
+      end
+    end
+
+    pkgs[_anything_(pkgs.keys)]
+  end
+
+  def self.packages(src, stack = [])
+    directories(src).reduce([]) do |pkgs, p|
+      pkgs << File.join(src, p)
+
+      if directories(File.join(src, p)).empty?
+        pkgs
+      else
+        pkgs + packages(File.join(src, p))
+      end
+    end
+  end
+
+  private
+
+  def self.directories path
+    Dir.entries(path).select do |filename|
+      File.directory?(File.join(path, filename))
+    end.select do |filename|
+      !filename.start_with?('.') && Dir.glob("**/*.go").empty?
+    end
   end
 end
